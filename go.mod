9bdfbb81f222bc301b473db7f18f678a0f23cb6d
ks_nh.92 <81375959+nhannguyen1003@users.noreply.github.com>
2022-09-16 23:12:49 +0700

    fisrt commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module first/module
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func add(x int, y int) int{
+	return x + y
+}
+
+func main() {
+    fmt.Printf("hello m.n, %d.\n" ,add(3,5 ))
+}
